6ec2cf483278fc2ff6589b2db7b51374f4cbff4d
Shreethaar Arunagirinathan <64171155+shreethaar@users.noreply.github.com>
2024-08-27 15:44:00 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var images = ["assets/avatar.png", "assets/avatar2.png"];
 var imgTag = document.getElementById("imgClickAndChange");
 var audio = new Audio("assets/music/resonance.mp3");
 audio.volume = 0.2;
-audio.loop = true;
+audio.loop = false;
 
 var audioPlayed = false;
 
