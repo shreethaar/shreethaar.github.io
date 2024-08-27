e2cd1e2bb75c4314a194e5fdefb2bacb4901284e
Shreethaar <shreethaara380@gmail.com>
2024-08-27 15:16:17 +0000

    onclick false second time

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
 var imgState = 0;
-var images = ["assets/avatar.png","assets/avatar2.png"];
-
+var images = ["assets/avatar.png", "assets/avatar2.png"];
 var imgTag = document.getElementById("imgClickAndChange");
+var audio = new Audio("assets/music/msc.mp3");
+audio.volume = 0.2;
+audio.loop = true;
+
+var audioPlayed = false;
 
 imgTag.addEventListener("click", function (event) {
   imgState = (++imgState % images.length);
   event.target.src = images[imgState];
 });
 
-var audio = new Audio("assets/music/resonance.mp3");
-audio.volume = 0.2;
-audio.loop = false;
+document.addEventListener("DOMContentLoaded", function() {
+  if (!audioPlayed) {
+    audio.play();
+    audioPlayed = true;
+  }
+});
 
 document.onclick = function() {
-  audio.play();
-}
+  if (!audioPlayed) {
+    audio.play();
+    audioPlayed = true;
+  }
+};
+
